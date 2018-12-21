813a4f372a67d85422f0f2d229e65b88053811e9
armasog <43734687+armasog@users.noreply.github.com>
2018-12-21 13:02:16 -0500

    Refactored Code for Simulator

--- simulator.py
+++ simulator.py
@@ -2,13 +2,33 @@ import engine, probability, config
 from statistics import mean
 import matplotlib.pyplot as plt
 
-number_of_sessions = 500
-trials = 100
-bet = 1
+number_of_trials = 5
+spins_per_trial = 50
+bet_per_spin = 1
 initial_bankroll = 100
-session_profits = []
-session_wins = []
+payout_per_trial = []
+profits_per_trial = []  # payout_per_trial - initial_bankroll
+wins_per_trial = []  # List of number of spins that resulted in a win for each trial
+average_payout_per_spin = []  # payout_per_trial / spins_per_trial
+average_payout_per_win = []  # payout_per_trial / wins_per_trial
 simulator = engine.player(initial_bankroll, 'auto')
+
+
+def calculate_summary_statistics(wins_for_trial, payout_for_trial, number_of_trials, spins_per_trial, initial_bankroll):
+    global profits_per_trial, wins_per_trial, average_payout_per_spin, average_payout_per_win
+    profit_for_trial = simulator.bankroll - initial_bankroll
+    profits_per_trial.append(profit_for_trial)
+    wins_per_trial.append(wins_for_trial)
+    payout_per_trial.append(payout_for_trial)
+    average_payout_per_spin.append(payout_for_trial / spins_per_trial)
+    if wins_for_trial != 0:
+        average_payout_per_win.append(payout_for_trial / wins_for_trial)
+    if wins_for_trial == 0:
+        average_payout_per_win.append(0)
+
+
+def print_summary_statistics():
+    pass
 
 print('Probabilities of each multiplier:')
 
@@ -17,22 +37,15 @@ for key in config.win_conditions:
         probability.probability_of_multiplier(config.win_conditions, config.symbols, config.win_conditions[key])))
 print('0 ' + str(probability.probability_of_loss(config.win_conditions, config.symbols, total_loss=True)))
 
-for _ in range(number_of_sessions):
-    session_win_count = 0
-    for _ in range(trials):
-        winnings = engine.calculate_winnings(engine.spin(), engine.place_bet(simulator, bet))
-        engine.pay_winnings(simulator, winnings)
-        if winnings > 0:
-            session_win_count += 1
-    profit = simulator.bankroll - initial_bankroll
-    session_profits.append(profit)
-    session_wins.append(session_win_count)
+for _ in range(number_of_trials):
+    wins_for_trial = 0
+    payout_for_trial = 0
+    for _ in range(spins_per_trial):
+        payout = engine.calculate_winnings(engine.spin(), engine.place_bet(simulator, bet_per_spin))
+        engine.pay_winnings(simulator, payout)
+        if payout > 0:
+            wins_for_trial += 1
+            payout_for_trial += payout
+    calculate_summary_statistics(wins_for_trial, payout_for_trial, number_of_trials, spins_per_trial, initial_bankroll)
     simulator.bankroll = initial_bankroll
 
-print('Profit: {}'.format(profit))
-print('Average profit: ' + str(mean(session_profits)))
-session = [i for i in range(1, number_of_sessions + 1)]
-# plt.scatter(session, session_wins)
-# plt.xlabel('Session')
-# plt.ylabel('Number of Wins')
-# plt.show()
